ad4ea11a4e1fd21cecb2d02844d94f1598b51186
svkylmz <Ylmazsevket@gmail.com>
2021-09-16 17:47:18 +0300

    Activate dagger-hilt dependency injection library

--- app/src/main/java/com/svkylmz/todoapp/MainActivity.kt
+++ app/src/main/java/com/svkylmz/todoapp/MainActivity.kt
@@ -3,13 +3,10 @@ package com.svkylmz.todoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.svkylmz.todoapp.ui.theme.TodoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/svkylmz/todoapp/di/DatabaseModule.kt
@@ -0,0 +1,26 @@
+package com.svkylmz.todoapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.svkylmz.todoapp.data.TodoDatabase
+import com.svkylmz.todoapp.util.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, TodoDatabase::class.java, DATABASE_NAME)
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: TodoDatabase) = database.todoDao()
+}
