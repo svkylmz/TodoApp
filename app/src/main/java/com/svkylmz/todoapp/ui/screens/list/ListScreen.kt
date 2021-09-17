f14e3f72b8e649292b7ff4a3216531d8fb3be4cb
svkylmz <Ylmazsevket@gmail.com>
2021-09-17 21:59:47 +0300

    Add list screen app bar actions and their dependencies

--- app/src/main/java/com/svkylmz/todoapp/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/svkylmz/todoapp/ui/screens/list/ListScreen.kt
@@ -1,6 +1,5 @@
 package com.svkylmz.todoapp.ui.screens.list
 
-import androidx.compose.foundation.background
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -8,7 +7,6 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
