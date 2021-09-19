99468a84291071e7e4a421941c545abc0bf65751
svkylmz <Ylmazsevket@gmail.com>
2021-09-19 19:27:30 +0300

    Fix some design features

--- app/src/main/java/com/svkylmz/todoapp/components/PriorityDropDownMenu.kt
+++ app/src/main/java/com/svkylmz/todoapp/components/PriorityDropDownMenu.kt
@@ -4,10 +4,7 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -22,7 +19,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.svkylmz.todoapp.data.models.Priority
 import com.svkylmz.todoapp.ui.theme.PRIORITY_DROP_DOWN_HEIGHT
@@ -46,7 +42,8 @@ fun PriorityDropDown(
             .clickable { expanded = true }
             .border(
                 width = 1.dp,
-                color = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.disabled)
+                color = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.disabled),
+                shape = MaterialTheme.shapes.small //4dp rounded corner same as outlined text field
             ),
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -74,7 +71,7 @@ fun PriorityDropDown(
             )
         }
         DropdownMenu(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth(fraction = 0.94f),
             expanded = expanded,
             onDismissRequest = { expanded = false }
         ) {
