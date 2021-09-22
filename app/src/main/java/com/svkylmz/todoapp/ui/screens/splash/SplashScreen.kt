1a6ef6e8dd8d421ef25d188284c4bcfbc0404655
svkylmz <Ylmazsevket@gmail.com>
2021-09-22 17:54:49 +0300

    Animate the splash screen

--- app/src/main/java/com/svkylmz/todoapp/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/svkylmz/todoapp/ui/screens/splash/SplashScreen.kt
@@ -1,18 +1,23 @@
 package com.svkylmz.todoapp.ui.screens.splash
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.svkylmz.todoapp.R
 import com.svkylmz.todoapp.ui.theme.SPLASH_LOGO_HEIGHT
 import com.svkylmz.todoapp.ui.theme.splashScreenBackgroundColor
@@ -22,18 +27,33 @@ import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navigateToListScreen: () -> Unit) {
+    var startAnimation by remember { mutableStateOf(false) }
+    val offsetState by animateDpAsState(
+        targetValue = if (startAnimation) 0.dp else 100.dp,
+        animationSpec = tween(durationMillis = 1000)
+    )
+    val alphaState by animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(durationMillis = 1000)
+    )
+    
     LaunchedEffect(key1 = true) {
+        startAnimation = true
         delay(SPLASH_SCREEN_DELAY)  //show the splash screen for 3 seconds
         navigateToListScreen()
     }
+    
     Box(
         modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.splashScreenBackgroundColor),
+            .fillMaxSize()
+            .background(MaterialTheme.colors.splashScreenBackgroundColor),
         contentAlignment = Alignment.Center
     ) {
         Image(
-            modifier = Modifier.size(SPLASH_LOGO_HEIGHT),
+            modifier = Modifier
+                .size(SPLASH_LOGO_HEIGHT)
+                .offset(y = offsetState)
+                .alpha(alpha = alphaState),
             painter = painterResource(id = R.drawable.ic_to_do),
             contentDescription = stringResource(id = R.string.todo_logo),
             colorFilter = ColorFilter.tint(MaterialTheme.colors.splashScreenContentColor)
